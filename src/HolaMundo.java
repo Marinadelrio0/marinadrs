195288eb7c66e49ee3589c7eec8b8caa510f4a2f
Marinadelrio0 <127426766+Marinadelrio0@users.noreply.github.com>
2025-05-01 12:42:48 +0200

    Update HolaMundo.java

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -6,5 +6,7 @@ public class HolaMundo {
 
     private static void saludar() {
         System.out.println("Hello world!");
+        System.out.println("¡Hola Mundo!");
+        System.out.println("Hallo Welt!");
     }
 }
