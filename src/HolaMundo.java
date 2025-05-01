5258c36bbc5a28246a96f3ceccdc06be3867fb83
Marinadelrio0 <marina.del.rio.sa@gmail.com>
2025-05-01 12:53:46 +0200

    Cambiar linea 10 para que diga ¿Cómo te llamas?
    Signed-off-by: Marinadelrio0 <marina.del.rio.sa@gmail.com>

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -7,6 +7,6 @@ public class HolaMundo {
     private static void saludar() {
         System.out.println("Hello world!");
         System.out.println("¡Hola Mundo!");
-        System.out.println("Hallo Welt!");
+        System.out.println("¿Cómo te llamas?");
     }
 }
