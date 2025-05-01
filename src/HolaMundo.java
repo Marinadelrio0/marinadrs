b5d7353fe345359f88ff5e254524de1d7d0ae4a2
Marinadelrio0 <marina.del.rio.sa@gmail.com>
2025-05-01 12:32:54 +0200

    Renombrar clase y extraer método
    Signed-off-by: Marinadelrio0 <marina.del.rio.sa@gmail.com>

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -1,6 +1,10 @@
-public class Main {
+public class HolaMundo {
     public static void main(String[] args) {
 
+        saludar();
+    }
+
+    private static void saludar() {
         System.out.println("Hello world!");
     }
 }
